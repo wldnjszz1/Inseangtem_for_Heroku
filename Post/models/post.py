4a1d195388ba300e77858a2f0d59c9569e265df9
wldnjszz1 <wldnjszz1@naver.com>
2019-08-10 00:57:51 +0900

    포스트 모델 변경

--- Post/models/post.py
+++ Post/models/post.py
@@ -5,16 +5,16 @@ class Post(models.Model):
     user_id = models.ForeignKey('Auth.IstUser', null=False, related_name="posts", on_delete=models.CASCADE)
     image = models.ImageField(null=False) #default='../../media/image.jpg',
     description = models.TextField()
-    category1 = models.BooleanField(null=True)
-    category2 = models.BooleanField(null=True)
-    category3 = models.BooleanField(null=True)
-    category4 = models.BooleanField(null=True)
-    category5 = models.BooleanField(null=True)
-    category6 = models.BooleanField(null=True)
-    category7 = models.BooleanField(null=True)
-    category8 = models.BooleanField(null=True)
-    category9 = models.BooleanField(null=True)
-    category10 = models.BooleanField(null=True)
+    category1 = models.BooleanField(default=False)
+    category2 = models.BooleanField(default=False)
+    category3 = models.BooleanField(default=False)
+    category4 = models.BooleanField(default=False)
+    category5 = models.BooleanField(default=False)
+    category6 = models.BooleanField(default=False)
+    category7 = models.BooleanField(default=False)
+    category8 = models.BooleanField(default=False)
+    category9 = models.BooleanField(default=False)
+    category10 = models.BooleanField(default=False)
 
     # def __str__(self):
     #     return str(self.id), self.user_id
